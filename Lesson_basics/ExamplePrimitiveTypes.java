79891ab2eac3e3be7eeee477006c17529d8c57a8
R00ney <ngo1991@gmail.com>
2013-08-23 14:53:26 -0400

    java lessons

---
+++ Lesson_basics/ExamplePrimitiveTypes.java
@@ -0,0 +1,19 @@
+class ExamplePrimitiveTypes {
+
+	public static void main(String[] args) {
+	
+		byte b= 127;
+		
+		short s= 257;
+		
+		double d= 3.4e13;
+		
+		char c = 'A';
+		
+		s++;
+		
+		System.out.println(String.valueOf(s));
+		
+	}
+	
+}
