ea41ed70414a8a61539ffb847a02e4521822bea5
R00ney <ngo1991@gmail.com>
2013-09-08 20:04:32 -0400

    compiled to 11

--- AmericanIdol/VoteForAmericanIdolClient.java
+++ AmericanIdol/VoteForAmericanIdolClient.java
@@ -60,8 +60,32 @@ public class VoteForAmericanIdolClient extends JApplet implements ActionListener
 	//Code that services GUI here
 	public void actionPerformed(ActionEvent ae) {
 	
-	System.out.println("Debug: Submit Pushed");
-	
+		System.out.println("Debug: Submit Pushed");
+		
+		String vote;
+		if (contestant1.isSelected()) vote = "1";
+		else if (contestant2.isSelected()) vote = "2";
+		else if (contestant3.isSelected()) vote = "3";
+		else if (contestant4.isSelected()) vote = "4";
+		else if (contestant5.isSelected()) vote = "5";
+		else    { // no radio button was selected...
+				msgLabel.setText("Select a contestant before SUBMIT");
+				msgLabel.setForeground(Color.red);
+				return; 
+				}       
+		System.out.println("Contestant #" + vote + " was selected.");
+
+		// Disable GUI objects so user
+		 // doesn't try to reselect or send again.
+		 submitButton.setEnabled(false);// disable button!
+		 contestant1.setEnabled(false); // disable button!
+		 contestant2.setEnabled(false); // disable button!
+		 contestant3.setEnabled(false); // disable button!
+		 contestant4.setEnabled(false); // disable button!
+		 contestant5.setEnabled(false); // disable button!
+
+		 msgLabel.setText(" "); //clear
+		 
 	} //end actionPerformed
 
 //No Main, because browser starts code
