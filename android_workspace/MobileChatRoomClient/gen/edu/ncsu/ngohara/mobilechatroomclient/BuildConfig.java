cf8d56e7dcfa02117d34c5f5d347e040f67b3c1c
R00ney <ngo1991@gmail.com>
2013-09-23 16:22:33 -0400

    cleanup

--- android_workspace/MobileChatRoomClient/gen/edu/ncsu/ngohara/mobilechatroomclient/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.ncsu.ngohara.mobilechatroomclient;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
