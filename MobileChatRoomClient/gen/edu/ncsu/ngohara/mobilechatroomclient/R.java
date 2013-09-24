40ecd66bb41f62b69771bf1d7d562c2714b97844
R00ney <ngo1991@gmail.com>
2013-09-23 23:06:19 -0400

    mext add create socket and connect server

--- MobileChatRoomClient/gen/edu/ncsu/ngohara/mobilechatroomclient/R.java
+++ MobileChatRoomClient/gen/edu/ncsu/ngohara/mobilechatroomclient/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080004;
+        public static final int errorTextField=0x7f080003;
         public static final int progressBar1=0x7f080002;
         public static final int signInButton=0x7f080001;
         public static final int usernameEdit=0x7f080000;
@@ -38,6 +39,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int error_textField=0x7f050004;
         public static final int hello_world=0x7f050002;
         public static final int sign_in=0x7f050003;
     }
--- MobileChatRoomClient/src/edu/ncsu/ngohara/mobilechatroomclient/SignInActivity.java
+++ MobileChatRoomClient/src/edu/ncsu/ngohara/mobilechatroomclient/SignInActivity.java
@@ -7,6 +7,8 @@ import android.app.Activity;
 import android.view.Menu;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.os.AsyncTask;
 
 
 public class SignInActivity extends Activity implements OnClickListener {
@@ -22,9 +24,13 @@ public class SignInActivity extends Activity implements OnClickListener {
         ProgressBar progressBar1 = (ProgressBar) findViewById(R.id.progressBar1);
         progressBar1.setVisibility(ProgressBar.INVISIBLE);
         
+        TextView errorTextField = (TextView) findViewById(R.id.errorTextField);
+        errorTextField.setVisibility(TextView.INVISIBLE);
+        
+        
     }
 
-
+ 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -41,6 +47,32 @@ public class SignInActivity extends Activity implements OnClickListener {
      
         
     }
+    
+    public class ConnectionWorker extends AsyncTask<String,Void,String> {
+        @Override
+        protected String doInBackground(String... arg0) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String val) {
+        	ProgressBar progressBar1 = (ProgressBar) findViewById(R.id.progressBar1);
+            progressBar1.setVisibility(ProgressBar.INVISIBLE);
+            Button signInButton = (Button) findViewById(R.id.signInButton);
+            signInButton.setEnabled(true);
+            
+            if(val.equals("Success")){
+            	System.out.println("Successfully logged in.");
+            
+            } else {
+            	System.out.println("Error: Could not log in.");
+                TextView errorTextField = (TextView) findViewById(R.id.errorTextField);
+                errorTextField.setText("Error, Could not Log in.");
+                errorTextField.setVisibility(TextView.VISIBLE);
+            }
+        }
+    }
+
     
     
 }
